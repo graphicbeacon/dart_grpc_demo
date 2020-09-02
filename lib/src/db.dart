ff450ac94d8b11c155c877f3dab8ebad6a6db9e5
Jermaine Oppong <jeropp00@gmail.com>
2020-09-02 23:38:40 +0100

    Add db and album proto messages

---
+++ lib/src/db.dart
@@ -0,0 +1,82 @@
+final albums = [
+  {'id': 1, 'title': 'quidem molestiae enim'},
+  {'id': 2, 'title': 'sunt qui excepturi placeat culpa'},
+  {'id': 3, 'title': 'omnis laborum odio'},
+  {'id': 4, 'title': 'non esse culpa molestiae omnis sed optio'},
+  {'id': 5, 'title': 'eaque aut omnis a'},
+];
+
+final photos = [
+  {
+    'albumId': 1,
+    'id': 27,
+    'title': 'sit asperiores est quos quis nisi veniam erras',
+    'url': 'https://via.placeholder.com/600/c984bf',
+  },
+  {
+    'albumId': 1,
+    'id': 28,
+    'title': 'non neque eligendi molestiae repudiandae illum',
+    'url': 'https://via.placeholder.com/600/392537',
+  },
+  {
+    'albumId': 2,
+    'id': 29,
+    'title': 'aut ipsam quos ab placeat omnis',
+    'url': 'https://via.placeholder.com/600/602b9e',
+  },
+  {
+    'albumId': 3,
+    'id': 30,
+    'title': 'odio enim voluptatem quidem aut nihil illum',
+    'url': 'https://via.placeholder.com/600/372c93',
+  },
+  {
+    'albumId': 3,
+    'id': 31,
+    'title': 'voluptate voluptates sequi',
+    'url': 'https://via.placeholder.com/600/a7c272',
+  },
+  {
+    'albumId': 3,
+    'id': 32,
+    'title': 'ad enim dignissimos voluptatem similique',
+    'url': 'https://via.placeholder.com/600/c70a4d',
+  },
+  {
+    'albumId': 4,
+    'id': 33,
+    'title': 'culpa ipsam nobis qui fuga magni et mollitia',
+    'url': 'https://via.placeholder.com/600/501fe1',
+  },
+  {
+    'albumId': 4,
+    'id': 34,
+    'title': 'vitae est facere quia itaque adipisci perfer',
+    'url': 'https://via.placeholder.com/600/35185e',
+  },
+  {
+    'albumId': 5,
+    'id': 35,
+    'title': 'tenetur minus voluptatum et',
+    'url': 'https://via.placeholder.com/600/c96cad',
+  },
+  {
+    'albumId': 5,
+    'id': 36,
+    'title': 'expedita rerum eaque',
+    'url': 'https://via.placeholder.com/600/4d564d',
+  },
+  {
+    'albumId': 5,
+    'id': 37,
+    'title': 'totam voluptas iusto deserunt dolores',
+    'url': 'https://via.placeholder.com/600/ea51da',
+  },
+  {
+    'albumId': 5,
+    'id': 38,
+    'title': 'natus magnam iure rerum pariatur molestias',
+    'url': 'https://via.placeholder.com/600/4f5b8d',
+  },
+];
